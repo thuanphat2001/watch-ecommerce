c1e917ec12c8f74f03aae1168b26f9bb9bfb450a
thuanphat2001 <95758970+thuanphat2001@users.noreply.github.com>
2022-08-31 23:25:52 +0700

    Product & ProductDetails #1

---
+++ components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Ecommerce Watch All rights reserved.</p>
+      <p className="icons">
+        <AiFillFacebook />
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
